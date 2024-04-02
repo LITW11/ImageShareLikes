f77ceb8aedd5f3224a48c627edccf8def748cefb
Avrumi Friedman <a.friedman07@gmail.com>
2024-04-01 21:50:44 -0400

    Add project files.

---
+++ ImageShareLikesEF.Data/Migrations/20240401234050_Remove junk columns.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageShareLikesEF.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removejunkcolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bar",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Baz",
+                table: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bar",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Baz",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
